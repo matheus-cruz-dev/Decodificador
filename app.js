6ae4794121413c888c284d0a4d47a69cbc635e19
matheus-cruz-dev <matheus.cruz.2004@hotmail.com>
2022-09-18 18:35:30 -0300

    first commit

--- app.js
+++ app.js
@@ -5,12 +5,21 @@ function btnEncript() {
 }
 
 function encript(stringEncripted) {
-  let matrizCode = [['e', 'enter',], ['i', 'imes'], ['a', 'ai'], ['o', 'ober'], ['u', 'ufat']]
+  let matrizCode = [
+    ['e', 'enter'],
+    ['i', 'imes'],
+    ['a', 'ai'],
+    ['o', 'ober'],
+    ['u', 'ufat']
+  ]
   stringEncripted = stringEncripted.toLowerCase()
 
   for (let i = 0; i < matrizCode.length; i++) {
     if (stringEncripted.includes(matrizCode[i][0])) {
-      stringEncripted = stringEncripted.replaceAll(matrizCode[i][0], matrizCode[i][1])
+      stringEncripted = stringEncripted.replaceAll(
+        matrizCode[i][0],
+        matrizCode[i][1]
+      )
     }
   }
   return stringEncripted
@@ -23,12 +32,21 @@ function btnDescript() {
 }
 
 function descript(stringDescripted) {
-  let matrizCode = [['e', 'enter',], ['i', 'imes'], ['a', 'ai'], ['o', 'ober'], ['u', 'ufat']]
+  let matrizCode = [
+    ['e', 'enter'],
+    ['i', 'imes'],
+    ['a', 'ai'],
+    ['o', 'ober'],
+    ['u', 'ufat']
+  ]
   stringDescripted = stringDescripted.toLowerCase()
 
   for (let i = 0; i < matrizCode.length; i++) {
     if (stringDescripted.includes(matrizCode[i][1])) {
-      stringDescripted = stringDescripted.replaceAll(matrizCode[i][1], matrizCode[i][0])
+      stringDescripted = stringDescripted.replaceAll(
+        matrizCode[i][1],
+        matrizCode[i][0]
+      )
     }
   }
   return stringDescripted
